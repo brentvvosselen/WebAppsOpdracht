3f20949d455be7cc906fafe7d38b94a70ed467d3
Brent Van Vosselen <brent.vanvosselen@live.be>
2017-11-01 12:48:58 +0100

    ANGULAR: toevoegen basis navigatie + fallback naar 404, en standaard route hij naar feed

--- web/bulkr/src/app/app.module.ts
+++ web/bulkr/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedComponent } from './feed/feed.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "feed", component:FeedComponent},
+      { path: "", component:FeedComponent},
+      { path: "**", component:NotFoundComponent}
+        
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
